2eca015decd2168a8bef709f43f25ad56862393a
Juan <juangpc@users.noreply.github.com>
2019-05-06 19:32:39 -0500

    adding dboff script file

---
+++ dboff.m
@@ -0,0 +1,22 @@
+% script DBOFF to debug a matlab project.
+%
+% Syntax:  script
+%         This script is just an alias for dbclear all. It clears all the
+%         breaking points enabled in matlab.
+%   
+% Inputs: 
+%    [No inputs]
+%
+% Outputs:
+%    [No outputs]
+%
+% Example: 
+%    >> dbon  
+%
+% Other m-files required: dbModeOn
+%
+% Author: Juan Garcia-Prieto
+% email: juangpc@gmail.com
+% May 2019; 
+%------------- BEGIN CODE --------------
+dbclear all
